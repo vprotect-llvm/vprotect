f0a05b6196efc6ed0ff0c9e346cda977a3a38d83
Damien Engels <engelsdamien@gmail.com>
2014-11-27 15:29:44 +0100

    New cpp example

---
+++ examples/VirtualMultipleFunctions.cpp
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+class IntSimple {
+public:
+    int a;
+};
+
+class CharSimple {
+public:
+    char a;
+};
+
+class LongSimple {
+public:
+    long a;
+};
+
+typedef int fun();
+class FunctionSimple {
+public:
+    long t;
+    fun** u;
+    virtual int a(int b){return b+3;}
+};
+
+class A {
+public:
+    int a;
+
+    virtual int x(){return 1;}
+    virtual char y(){return 11;}
+    virtual long z(){return 111;}
+
+};
+
+class B: public A {
+public:
+    int b;
+    virtual int x(){return 2;}
+    virtual char y(){return 22;}
+    virtual long z(){return 222;}
+};
+
+int stoopid(){
+    return 42;
+}
+
+int main(void){
+    IntSimple* a = new IntSimple();
+    CharSimple* b = new CharSimple();
+    LongSimple* c = new LongSimple();
+    FunctionSimple* d = new FunctionSimple();
+    fun* t = &stoopid;
+    d->u = &t;
+    int e = d->a(a->a);
+
+    A* var = new B();
+    int x = var->x();
+    char y = var->y();
+    long z = var->z();
+
+    printf("%d %d %ld\n", x, y, z);
+    delete var;
+    return 0;
+}
