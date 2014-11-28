e64b5896b7fecd7587d9dbba685de4834a1403ff
Damien Engels <engelsdamien@gmail.com>
2014-11-28 01:49:05 +0100

    New program with a complex hierarchy added to examples

---
+++ examples/LargeHierarchy.cpp
@@ -0,0 +1,22 @@
+class A {
+    virtual int a(){return 1;}
+};
+class B : public A {
+    virtual int a(){return 2;}
+};
+class C : public A {
+    virtual int a(){return 3;}
+};
+class D : public C {
+    virtual int a(){return 4;}
+};
+class E : public D {
+    virtual int a(){return 5;}
+};
+class F : public D {
+    virtual int a(){return 6;}
+};
+class G : public C {
+    virtual int a(){return 7;}
+};
+
