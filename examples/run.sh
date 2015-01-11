bb8c3548e0b6b7d02ec57f4ec5f7796400b57afc
Damien Engels <engelsdamien@gmail.com>
2015-01-11 12:36:43 +0100

    Compress output if it is the same in both cases

--- examples/run.sh
+++ examples/run.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
 make -j16
-for i in $(echo *.cpp | tr ' ' '\n' | cut -d'.' -f1); do 
-    echo "---- $i ----";
-    ./build/$i.bin; 
-    echo "==> returned : $?"; 
-    echo "------------";
-    ./build/$i.mod.bin; 
-    echo "==> returned : $?"; 
-    echo ""; 
+for i in $(echo *.cpp | tr ' ' '\n' | cut -d'.' -f1); do
+    echo -n "---- $i ----";
+
+    out1=$(./build/$i.bin 2>&1);
+    ret1=$?
+
+    out2=$(./build/$i.mod.bin 2>&1);
+    ret2=$?
+
+    if [ "$out1" = "$out2" -a $ret1 -eq $ret2 ]; then
+        echo " => Results are equal "
+    else
+        echo ""
+        echo "$out1"
+        echo "==> returned : $ret1";
+        echo "------------";
+        echo "$out2"
+        echo "==> returned : $ret2";
+        echo "";
+    fi
 done
 
